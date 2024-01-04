5de6142f3abb08826f754339c3f0565315a7373c
Aliaghaei1991 <155421331+Aliaghaei1991@users.noreply.github.com>
2024-01-04 11:45:21 +0330

    insert Factorial

--- Test_git/Test_git/Program.cs
+++ Test_git/Test_git/Program.cs
@@ -15,8 +15,17 @@ namespace Test_git
 
             string sum = ((5928000) - (1 / 100) * 9).ToString();
 
-            
 
+            int n = 5;
+            int sumf = 1;
+
+           for(int i = n; i >=1; i--)
+            {
+                sumf *= i;
+            }
+
+
+            Console.WriteLine(sumf);
             Console.ReadKey();
         }
     }
